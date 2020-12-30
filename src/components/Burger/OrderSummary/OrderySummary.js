b274df341007a15352d9e0e50fdaf081979d1813
kosos <kosos@mitre.org>
2020-12-30 12:02:00 -0500

    Fix folder structure and improve efficiency of Modal updating

--- src/components/Burger/OrderSummary/OrderySummary.js
+++ src/components/Burger/OrderSummary/OrderySummary.js
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(
-    (ingredientKey) => {
-      return (
-        <li key={ingredientKey}>
-          <span style={{ textTransform: "capitalize" }}>{ingredientKey}</span>:{" "}
-          {props.ingredients[ingredientKey]}
-        </li>
-      );
-    }
-  );
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
+  // This could just be a functional component
+  // This function helps us ensure OrderSummary only updates when the modal is visible
+  componentDidUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (ingredientKey) => {
+        return (
+          <li key={ingredientKey}>
+            <span style={{ textTransform: "capitalize" }}>{ingredientKey}</span>
+            : {this.props.ingredients[ingredientKey]}
+          </li>
+        );
+      }
+    );
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/Aux/Aux";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
@@ -17,7 +17,7 @@ class BurgerBuilder extends Component {
   state = {
     ingredients: {
       salad: 0,
-      bacon: 0,
+    bacon: 0,
       cheese: 0,
       meat: 0,
     },
